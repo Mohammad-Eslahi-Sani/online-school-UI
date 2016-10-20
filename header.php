9352471e00318e843b05721b35be70c4504ca19f
mohamad eslahi sani <mohammad-eslahi-sani@yahoo.com>
2016-10-20 21:21:56 +0330

    [INIT] creating basics
    file organization has been setteled for starting development

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php
+    include_once "config.php";
+?>
+<!DOCTYPE html>
+<html >
+<head>
+    <!-- js libraries:  -->
+    <script type="text/javascript" src="libs/js/jquery-2.2.0.min.js"></script>
+
+    <!--     -->
+
+    <!-- css libraries   -->
+    <link href="libs/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+    <!--     -->
+    <link href="resources/css/header/header.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body dir="auto">
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <!-- js libraries:  -->
+    <!--     -->
+
+    <!-- css libraries   -->
+    
+    <!--     -->
+    
+</head>
+
+<body dir="auto">
+
+</body>
+
+</html>
